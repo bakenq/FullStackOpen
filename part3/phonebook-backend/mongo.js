eb22419576d6d6720155e126ad322c24f0753f3e
Niko <nikolasm@outlook.de>
2025-04-08 21:29:06 +0200

    exercise 3.12

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://bakenq:${password}@fso-cluster.9duwnkc.mongodb.net/?retryWrites=true&w=majority&appName=fso-cluster`
+const url = `mongodb+srv://bakenq:${password}@fso-cluster.9duwnkc.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fso-cluster`
 
 mongoose.set('strictQuery', false)
 
@@ -23,14 +23,54 @@ mongoose.connect(url)
     .then(() => {
         console.log('connected')
 
+        // List phonebook entries
         if (process.argv.length === 3) {
-            return Person.find({})
-                .then(result => {
+            Person.find({})
+                .then(persons => {
                     console.log('phonebook:')
-                    result.forEach(person => {
+                    persons.forEach(person => {
                         console.log(`${person.name} ${person.number}`)
                     })
+                    return mongoose.connection.close()
+                })
+                .then(() => {
+                    console.log('connection closed')
+                })
+                .catch(err => {
+                    console.error('Error fetching persons:', err)
                     mongoose.connection.close()
                 })
+
+            // Add a new person to the phonebook
+        } else if (process.argv.length === 5) {
+            const name = process.argv[3]
+            const number = process.argv[4]
+
+            const person = new Person({
+                name: name,
+                number: number,
+            })
+
+            person.save()
+                .then(savedPerson => {
+                    console.log(`added ${savedPerson.name} number ${savedPerson.number} to phonebook`)
+                    return mongoose.connection.close()
+                })
+                .then(() => {
+                    console.log('connection closed')
+                })
+                .catch(err => {
+                    console.error('Error saving person:', err)
+                    mongoose.connection.close()
+                })
+
+            // Invalid number of arguments
+        } else {
+            console.log('Invalid number of arguments. Usage: node mongo.js <password> "<name>" <number>')
+            mongoose.connection.close().then(() => process.exit(1))
         }
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err.message)
+        process.exit(1)
     })
