657c4ae90931b25dcb704477d0cd0b1870fc60f0
Niko <nikolasm@outlook.de>
2025-04-09 18:30:51 +0200

    exercise 4.1 + 4.2

---
+++ part4/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,33 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogsRouter.get('/', (request, response, next) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+  .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title or url missing' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  })
+
+  blog.save()
+  .then(savedBlog => {
+    response.json(savedBlog)
+  })
+  .catch(error => next(error))
+})
+
+module.exports = blogsRouter
