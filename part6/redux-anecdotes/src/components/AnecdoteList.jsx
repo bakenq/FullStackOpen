5180101fc9722213739208390a2935adb47cb9b5
Niko <92971939+bakenq@users.noreply.github.com>
2025-04-14 18:05:09 +0200

    exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -10,7 +11,11 @@ const AnecdoteList = () => {
     dispatch(voteAnecdote(id))
   }
 
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  const anecdotesToShow = anecdotes.filter(anecdote =>
+    anecdote.content.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const sortedAnecdotes = [...anecdotesToShow].sort((a, b) => b.votes - a.votes)
 
   return (
     <>
---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,26 @@
+const initialState = ''
+
+// Actions
+const SET_FILTER = 'SET_FILTER'
+
+const filterReducer = (state = initialState, action) => {
+  console.log('ACTION (filter)', action)
+
+  switch (action.type) {
+    case SET_FILTER:
+      return action.payload
+
+    default:
+      return state
+  }
+}
+
+export const setFilter = (filter) => {
+  return {
+    type: SET_FILTER,
+    payload: filter
+  }
+}
+
+
+export default filterReducer
