6ac7dcf9eacb0556a7052c0a1970b290cf64d867
Niko <92971939+bakenq@users.noreply.github.com>
2025-04-15 12:10:02 +0200

    exercise 6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -6,3 +6,14 @@ export const getAnecdotes = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
+
+export const createAnecdote = async (content) => {
+  const object = { content, votes: 0 }
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
+
+export const updateAnecdote = async (updatedAnecdote) => {
+  const response = await axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
+  return response.data
+}
