2f1f2f2870f776784a77f92c4f8ad47f967a3325
Niko <92971939+bakenq@users.noreply.github.com>
2025-04-19 20:08:25 +0200

    exercise 9.5

--- part9/ts-exercises/src/bmiCalculator.ts
+++ part9/ts-exercises/src/bmiCalculator.ts
@@ -1,4 +1,13 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
 const calculateBmi = (height: number, weight: number): string => {
+  if (height <= 0 || weight <= 0) {
+    throw new Error("Height and weight must be positive numbers!");
+  }
+
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
 
@@ -13,11 +22,6 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-interface BmiValues {
-  height: number;
-  weight: number;
-}
-
 const parseBmiArguments = (args: string[]): BmiValues => {
   if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
@@ -26,23 +30,24 @@ const parseBmiArguments = (args: string[]): BmiValues => {
   const weight = Number(args[3]);
 
   if (!isNaN(height) && !isNaN(weight)) {
-    return {
-      height: height,
-      weight: weight,
-    };
+    return { height, weight };
   } else {
     throw new Error("Provided values were not numbers!");
   }
 };
 
-try {
-  const { height, weight } = parseBmiArguments(process.argv);
-  const result = calculateBmi(height, weight);
-  console.log(result);
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong: ";
-  if (error instanceof Error) {
-    errorMessage += error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseBmiArguments(process.argv);
+    const result = calculateBmi(height, weight);
+    console.log(result);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong: ";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.error(errorMessage);
   }
-  console.error(errorMessage);
 }
+
+export default calculateBmi;
--- part9/ts-exercises/src/index.ts
+++ part9/ts-exercises/src/index.ts
@@ -1,8 +1,39 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
+
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const heightStr = req.query.height;
+  const weightStr = req.query.weight;
+
+  if (typeof heightStr !== "string" || typeof weightStr !== "string") {
+    res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const height = Number(heightStr);
+  const weight = Number(weightStr);
+
+  if (isNaN(height) || isNaN(weight)) {
+    res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  try {
+    const bmiResult = calculateBmi(height, weight);
+
+    res.json({
+      weight: weight,
+      height: height,
+      bmi: bmiResult,
+    });
+  } catch (error: unknown) {
+    res.status(400).json({ error: "malformatted parameters" });
+  }
 });
 
 const PORT = 3003;
