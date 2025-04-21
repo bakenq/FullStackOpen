949576dd9cbba49f1290c59848448818db2b5645
Niko <92971939+bakenq@users.noreply.github.com>
2025-04-21 18:36:06 +0200

    exercise 9.11

---
+++ part9/patientor/backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientsData from "../../data/patients";
+import { Patient, NonSensitivePatient } from "../types";
+
+const patients: Patient[] = patientsData;
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients,
+};
