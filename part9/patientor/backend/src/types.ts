0f8441d514ec0123549b3689e53100cf19fcf6a6
Niko <92971939+bakenq@users.noreply.github.com>
2025-04-23 18:03:05 +0200

    exercise 9.23

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -4,14 +4,54 @@ export interface Diagnosis {
   latin?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare"; // Literal type field
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital"; // Literal type field
+  discharge: {
+    // Discharge object
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export enum Gender {
   Male = "male",
   Female = "female",
   Other = "other",
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
 
 export interface Patient {
   id: string;
